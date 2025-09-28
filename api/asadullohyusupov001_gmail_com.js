450c750874e641a74dcbfd5ef0bb90b37ff8408d
harryhackgo <mrharrypotter713@gmail.com>
2025-09-28 13:18:28 +0500

    Patched the api

--- api/asadullohyusupov001_gmail_com.js
+++ api/asadullohyusupov001_gmail_com.js
@@ -1,12 +1,19 @@
-module.exports = (req, res) => {
-  const q = require("url").parse(req.url, true).query;
-  const xs = q.x + "";
-  const ys = q.y + "";
+// api/asadulloh_mail_com.js
+export default function handler(req, res) {
+  const q = new URL(req.url, `http://${req.headers.host}`).searchParams;
+  const xs = q.get("x");
+  const ys = q.get("y");
+
+  // Natural number validation
   const nat = (s) => /^[1-9]\d*$/.test(s);
+
   res.setHeader("content-type", "text/plain; charset=utf-8");
+
   if (!nat(xs) || !nat(ys)) return res.end("NaN");
+
   let a = BigInt(xs),
     b = BigInt(ys);
+
   const gcd = (m, n) => {
     while (n) {
       const t = m % n;
@@ -15,7 +22,9 @@ module.exports = (req, res) => {
     }
     return m;
   };
+
   const g = gcd(a, b);
   const l = (a / g) * b;
+
   res.end(l.toString());
-};
+}
